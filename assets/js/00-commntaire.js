44e435cd196e444c46c8aadfe6d3df26c880b6bd
manoj <gnanavel.manoj29@gmail.com>
2024-03-04 11:19:52 +0100

    first commit

---
+++ assets/js/00-commntaire.js
@@ -0,0 +1,8 @@
+// un commentaire sur une ligne en javascript
+/*
+
+    Un commentaire multiligne 
+    en javascript
+
+
+*/
---
+++ assets/js/01-variable.js
@@ -0,0 +1,32 @@
+/* ---------------------------------Variables------------------------------------ */
+
+// ? NB : les noms se variables sont sensibles à la casse
+// On fait la difference  entre les majuscules et les miniscules
+
+// Déclaration de Variable
+let uneVariable;
+
+// Affectation d'une Variable (donner une valeur à la variable)
+uneVariable = "Je suis une Variable";
+
+// Les deux peuvent se faire en meme temps
+let hello = "Hello World";
+uneVariable = hello;
+
+console.log(hello);
+
+
+// Le mot clé const permet de déclarer une constante 
+// Vous ne pouvez pas modifier une constante (on dit qu'elle est immuable)
+// `const ` va garantir que la valeur ne soit pas modifié tout au long de l'execution du script
+const constante = "autre chose";
+constante = "autre chose";
+
+// le mot clé permet aussi de déclarer des variables 
+// C'est une pratiqué désuète (c'etait la seule façon avant 2015)
+var past = 'je suis dépassé'
+
+
+
+
+
