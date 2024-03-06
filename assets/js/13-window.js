0f30359d715c85a28e52a121881ec68478c6a65e
manoj <gnanavel.manoj29@gmail.com>
2024-03-06 16:49:39 +0100

    add window.js

---
+++ assets/js/13-window.js
@@ -0,0 +1,17 @@
+/*--------------------------WINDOW-------------------------- */
+
+/*
+    l'objet window en javascript est un objet global qui represente la fenêtre du navigateur
+    Cet objet est crée automatiquement lorsque la page web est chargée
+*/
+
+
+// L'objet window est un objet inportant en javascript car il fournit une interface entre le code javascript et le fenetre du navigateur 
+console.log(window);
+// la propriété `window.console.log()` permet d'accéder à l'objet console du navigateur
+console.log(window.console.log('hello world'));
+
+// la propriété `window.alert()`permet d'afficher une boite de dialogue dans la fenêtre du navigateur
+console.log(window.alert('hello world'));
+
+
