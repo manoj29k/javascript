5e97752b867702bda0586e1218ccf6fa6ce6874d
manoj <gnanavel.manoj29@gmail.com>
2024-03-08 13:49:31 +0100

    commit

--- assets/js/16-practice-list.js
+++ assets/js/16-practice-list.js
@@ -1,23 +1,26 @@
-const listMovies = document.querySelector('#list-movies');
+const listMovies = document.querySelector("#list-movies");
 // listMovies.style.backgroundColor = 'red'
 // listMovies.style.padding = '30px'
 
-listMovies.addEventListener('click', function (event) {
-    if (
-        event.target.style.fontSize == '' ||
-        event.target.style.fontSize == '16px'
-    ) {
-        event.target.style.fontSize = '30px';
-    } else {
-        event.target.style.fontSize = '16px';
-    }
+listMovies.addEventListener("click", function (event) {
+  if (
+    event.target.style.fontSize == "" ||
+    event.target.style.fontSize == "16px"
+  ) {
+    event.target.style.fontSize = "30px";
+  } else {
+    event.target.style.fontSize = "16px";
+  }
 });
 
-listMovies.addEventListener('dblclick', function (e) {
-    const title = document.createElement('h2');
-    title.textContent = e.target.textContent;
+// .remove() permret de supprimer un élément
 
-    const container = document.querySelector('#container');
+listMovies.addEventListener("dblclick", function (e) {
+  const title = document.createElement("h2");
+  title.textContent = e.target.textContent;
 
-    container.prepend(title);
+  const container = document.querySelector("#container");
+
+  container.prepend(title);
+//   title.remove(e.target);
 });
